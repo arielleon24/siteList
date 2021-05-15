b7b1c80d9a8f3b0aff93316ae57e9a868de7a5a2
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-05-14 22:39:03 -0400

    nav mostly done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,32 @@
 import React from "react";
+// import { ArrowDropDownIcon, SubjectIcon } from '@material-ui/icons';
+import AppsIcon from '@material-ui/icons/Apps';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import Button from "@material-ui/core/Button";
+import SearchIcon from '@material-ui/icons/Search';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
 export default function Nav(props) {
   return (
     <nav className="navbar">
+      <div className="navtop">
+      <DehazeIcon className="navIcons" />
       <h1>Scheduling</h1>
+      <section className="topRight">
+      <AppsIcon className="navIcons" />
+      <div className="roundInitial">D</div>
+      </section>
+      </div>
       <div className="navmid">
-        {" "}
-        <h2>Sites</h2>{" "}
+        <h2>Sites</h2>
       </div>
       <footer className="navfoot">
         <h3>All sites</h3>
-        <p>down arrow</p>
+        <ArrowDropDownIcon />
         <div className="navSideIcons">
-          <div>lil triangles </div>
-          <div> looking glass</div>
+          <FilterListIcon />
+          <SearchIcon />
         </div>
       </footer>
     </nav>
