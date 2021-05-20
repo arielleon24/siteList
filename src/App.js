abf8c30700e5f896435b9eae13262ddf09280ccc
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-05-19 23:36:18 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import './App.css';
 import Nav from './components/Nav'
 import List from './components/List'
 import Footer from './components/Footer'
+import { createBrowserHistory } from 'history';
+
+//REACT router for ItemView page
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <div className="animate-background">
-      <List />
-      </div>
-      <Footer />
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route path="/">
+          <div className="App">
+            <Nav />
+            <div className="animate-background">
+            <List />
+            </div>
+            <Footer />
+          </div>
+        </Route>
+      </Switch>
+    </ Router >
   );
 }
 
